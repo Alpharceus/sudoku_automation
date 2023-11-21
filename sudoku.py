45c6223c42b997701ea63e4c9cc4087323113d25
Alpharceus <72083371+Alpharceus@users.noreply.github.com>
2023-11-21 16:57:20 -0700

    Create sudoku.py

---
+++ sudoku.py
@@ -0,0 +1,52 @@
+def is_valid(board, row, col, num):
+    # Check if the number is not repeated in the row, column, and 3x3 subgrid
+    for x in range(9):
+        if board[row][x] == num or board[x][col] == num or board[3 * (row // 3) + x // 3][3 * (col // 3) + x % 3] == num:
+            return False
+    return True
+
+def solve_sudoku(board):
+    empty = find_empty_location(board)
+    if not empty:
+        return True  # Puzzle solved
+    row, col = empty
+
+    for num in range(1, 10):
+        if is_valid(board, row, col, num):
+            board[row][col] = num
+
+            if solve_sudoku(board):
+                return True
+
+            board[row][col] = 0  # Reset the cell
+
+    return False  # Triggers backtracking
+
+def find_empty_location(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return i, j
+    return None
+
+def print_board(board):
+    for row in board:
+        print(" ".join(str(num) for num in row))
+
+# Example Sudoku puzzle
+board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+if solve_sudoku(board):
+    print_board(board)
+else:
+    print("No solution exists")
